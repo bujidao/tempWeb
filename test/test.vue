532455d2e5075481f8fa20bb088e6bfa9043b598
lixf932 <>
2018-09-20 09:18:06 +0800

    test

---
+++ test/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'i am test'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" ref="stylesheet/stylus">
+
+</style>
