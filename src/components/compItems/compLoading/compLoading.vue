a5d7e3cbee4c34956a53c9d8a00ab0235945d0dc
lixf932 <>
2018-09-26 17:26:43 +0800

    loading

--- src/components/compItems/compLoading/compLoading.vue
+++ src/components/compItems/compLoading/compLoading.vue
@@ -3,11 +3,15 @@
     <div class="t-comp-wrapper">
       <Style1></Style1>
     </div>
+    <div class="t-comp-wrapper">
+      <Style2></Style2>
+    </div>
   </div>
 </template>
 
 <script>
   import Style1 from './styles/style1/demo'
+  import Style2 from './styles/style2/demo'
   export default {
     data () {
       return {
@@ -15,7 +19,8 @@
       }
     },
     components: {
-      Style1
+      Style1,
+      Style2
     }
   }
 </script>
